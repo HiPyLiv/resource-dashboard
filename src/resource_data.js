dcb94e398e0d01556c603dba3fcc5a540448c5b0
Sam Ball <stmball@gmail.com>
2021-05-27 10:51:45 +0100

    adding links to buttons;

--- src/resource_data.js
+++ src/resource_data.js
@@ -9,19 +9,19 @@ const resource_data = {
                     "id": 0,
                     "title": "Getting Started with Python 1",
                     "description": "An introduction to fundamental programing concepts in Python",
-                    "link": ""
+                    "link": "https://nbviewer.jupyter.org/github/HiPyLiv/HiPyProject/blob/overhaul/resources/python/basics/%5BBASICS%5D%20Getting%20Started%20With%20Python%201%20-%20The%20Basics/Getting%20Started%20In%20Python%201%20-%20The%20Basics.ipynb"
                 },
                 "Getting Started 2":  {
                     "id": 1,
                     "title": "Getting Started with Python 2",
                     "description": "The next steps with Python - getting Python to automate the boring bits!",
-                    "link": ""
+                    "link": "https://nbviewer.jupyter.org/github/HiPyLiv/HiPyProject/tree/overhaul/resources/python/basics/%5BBASICS%5D%20Getting%20Started%20With%20Python%202%20-%20The%20Basics/"
                 },
                 "Numpy and Pandas": {
                     "id": 2,
                     "title": "numpy and pandas",
                     "description": "Now it's time to learn two of the most powerful Python libraries - numpy and pandas.",
-                    "link": ""
+                    "link": "https://nbviewer.jupyter.org/github/HiPyLiv/HiPyProject/blob/overhaul/resources/python/basics/%5BBASICS%5D%20Numpy%20and%20Pandas/Numpy%20and%20Pandas.ipynb"
                 }
             }
         },
@@ -34,79 +34,73 @@ const resource_data = {
                     "id": 0,
                     "title": "Getting Started",
                     "description": "Learn how to get started with Python in your favourite programming environment",
-                    "link": ""
+                    "link": "https://nbviewer.jupyter.org/github/HiPyLiv/HiPyProject/blob/overhaul/resources/python/pure/PurePy%200.%20Woffle%20and%20Getting%20Started/0.1%20Introduction.ipynb"
                 },
                 "Numbers and Variables": {
                     "id": 1,
                     "title": "Numbers and Variables",
                     "description": "A deep dive into numbers and variables in Python",
-                    "link": ""
+                    "link": "https://nbviewer.jupyter.org/github/HiPyLiv/HiPyProject/blob/overhaul/resources/python/pure/PurePy%201.%20Numbers%20and%20Variables/Numbers%20and%20variables.ipynb"
                 },
                 "Logic and Flow": {
                     "id": 2,
                     "title": "Logic and Flow",
                     "description": "An introduction into using control flow and logical operators with Python",
-                    "link": ""
+                    "link": "https://nbviewer.jupyter.org/github/HiPyLiv/HiPyProject/blob/overhaul/resources/python/pure/PurePy%202.%20Logic%20and%20Flow/2.0%20Logic%20and%20Flow.ipynb"
                 },
                 "Importing Modules": {
                     "id": 3,
                     "title": "Importing Modules",
                     "description": "An overview in how modules and project structure works in Python",
-                    "link": ""
+                    "link": "https://nbviewer.jupyter.org/github/HiPyLiv/HiPyProject/blob/overhaul/resources/python/pure/PurePy%203.%20Importing%20modules/Importing%20more%20functions.ipynb"
                 },
                 "Defining Functions": {
                     "id": 4,
                     "title": "Defining Functions",
                     "description": "We take a look at how to define our own functions, some common pitfalls in doing so and some best practices",
-                    "link": ""
+                    "link": "https://nbviewer.jupyter.org/github/HiPyLiv/HiPyProject/blob/overhaul/resources/python/pure/PurePy%204.%20Defining%20Our%20Own%20Functions/Defining%20our%20own%20functions.ipynb"
                 },
                 "Data Structures": {
                     "id": 5,
                     "title": "Data Structures",
                     "description": "Some of the native Python data structures we can use for our projects",
-                    "link": ""
+                    "link": "https://nbviewer.jupyter.org/github/HiPyLiv/HiPyProject/blob/overhaul/resources/python/pure/PurePy%205.%20Data%20Structures/5.0%20Data%20Structures.ipynb"
                 },
                 "Command Line Interlude": {
                     "id": 6,
                     "title": "The Command Line",
                     "description": "We take a break from Python to take a look at the command line, and how to use it to make our lives easier.",
-                    "link": ""
+                    "link": "https://nbviewer.jupyter.org/github/HiPyLiv/HiPyProject/blob/overhaul/resources/python/pure/PurePy%205.5%20Command-line%20interlude/5.5%20The%20command%20line.ipynb"
                 },
                 "Strings and Text": {
                     "id": 7,
                     "title": "Strings and Text",
                     "description": "Python is fantastic at manipulating, chopping up and searching for text in a file. In this guide, we take a look how to do that.",
-                    "link": ""
+                    "link": "https://nbviewer.jupyter.org/github/HiPyLiv/HiPyProject/blob/overhaul/resources/python/pure/PurePy%206.%20Strings%20and%20Text/6.0%20Working%20with%20text.ipynb"
                 },
                 "Importing Data": {
                     "id": 8,
                     "title": "Importing Data",
                     "description": "Data comes in all sizes, shapes and formats. Here we look at how we can use Python to import data for use in our code.",
-                    "link": ""
+                    "link": "https://nbviewer.jupyter.org/github/HiPyLiv/HiPyProject/blob/overhaul/resources/python/pure/PurePy%207.%20Importing%20Data/7.1%20Opening%20Files.ipynb"
                 },
                 "Advanced Control Flow": {
                     "id": 9,
                     "title": "Advanced Control Flow",
                     "description": "We revisit the previous work on control flow and dig a little deeper!",
-                    "link": ""
+                    "link": "https://nbviewer.jupyter.org/github/HiPyLiv/HiPyProject/blob/overhaul/resources/python/pure/PurePy%208.%20Advanced%20Flow%20Control/8.0%20Advanced%20Flow%20Control.ipynb"
                 },
                 "Advanced Functions": {
                     "id": 10,
                     "title": "Advanced Functions",
                     "description": "We take another look at functions, and how to get the most out of them.",
-                    "link": ""
+                    "link": "https://nbviewer.jupyter.org/github/HiPyLiv/HiPyProject/blob/overhaul/resources/python/pure/PurePy%209.%20Advanced%20Functions/9.0%20Advanced%20Functions%20and%20Functional%20Programming.ipynb"
                 },
                 "Object Orientated Programming" : {
                     "id": 11,
-                    "title": "Object Orientated Programming Part 1",
+                    "title": "Object Orientated Programming",
                     "description": "Object Orientated Programming is a common programming paradigm. In this resource we take a look at it, and begin to use its principles to become master programmers.",
-                    "link": ""
-                },
-                "Object Orientated Programming 2": {
-                    "id": 12,
-                    "title": "Object Orientated Programming Part 2",
-                    "description": "OOP is a huge topic, so we had to split it over two guides. Here we finish off looking at what makes OOP so great!",
-                    "link": ""
+                    "link": "https://nbviewer.jupyter.org/github/HiPyLiv/HiPyProject/blob/overhaul/resources/python/pure/PurePy%2010.%20Object-Oriented%20Programming/10.1%20Classes%20and%20objects.ipynb"
                 }
             }
         },
@@ -119,25 +113,25 @@ const resource_data = {
                     "id": 0,
                     "title": "A Deeper Look at matplotlib",
                     "description": "We take a deeper look at the grandfather Python visualisation library: matplotlib",
-                    "link": ""
+                    "link": "https://nbviewer.jupyter.org/github/HiPyLiv/HiPyProject/blob/overhaul/resources/python/visualisation/%5BVISUALISATION%5D%20A%20Deeper%20Look%20Into%20Matplotlib/A%20Deeper%20Look%20Into%20Matplotlib.ipynb"
                 },
                 "Pandas Builtin": {
                     "id": 1,
                     "title": "Built-in pandas Plotting",
                     "description": "Sometimes we want to make a super quick plot without doing a lot of work. Enter pandas' plotting functionality!",
-                    "link": ""
+                    "link": "https://nbviewer.jupyter.org/github/HiPyLiv/HiPyProject/blob/overhaul/resources/python/visualisation/%5BVISUALISATION%5D%20Panda%27s%20Built%20In%20Vis/Pandas%20Built%20In%20Vis.ipynb"
                 },
                 "Seaborn": {
                     "id": 2,
                     "title": "Statistical Plotting with seaborn",
                     "description": "seaborn allows super suave statistical plots in very little code. Here we look at how to get neat plots from seaborn.",
-                    "link": ""
+                    "link": "https://nbviewer.jupyter.org/github/HiPyLiv/HiPyProject/blob/overhaul/resources/python/visualisation/%5BVISUALISATION%5D%20Statistical%20Plotting%20With%20Seaborn/Statistical%20Plotting%20With%20Seaborn.ipynb"
                 },
                 "Bokeh": {
                     "id": 3,
                     "title": "Interactive Plotting with Bokeh",
                     "description": "We look at how to make interactive, dynamic plots using bokeh - a library that translates Python code into d3.js visualisations",
-                    "link": ""
+                    "link": "https://nbviewer.jupyter.org/github/HiPyLiv/HiPyProject/tree/overhaul/resources/python/visualisation/%5BVISUALISATION%5D%20Interactive%20Plots%20With%20Bokeh/"
                 }
             }
         },
@@ -150,13 +144,13 @@ const resource_data = {
                     "id": 0,
                     "title": "Sourcing Data from Files",
                     "description": "We look at some of the neat ways Python can help us import data from common file types, by using vanilla Python as well as libraries.",
-                    "link": ""
+                    "link": "https://nbviewer.jupyter.org/github/HiPyLiv/HiPyProject/blob/overhaul/resources/python/sourcing/%5BSOURCING%5D%20Importing%20Data%20From%20Files/Importing%20Files%20From%20Files.ipynb"
                 },
                 "Data from Web": {
                     "id": 1,
                     "title": "Sourcing Data from the Web",
                     "description": "Sometimes we don't have the data at hand, and want to turn to the internet to gather some. We look at some neat ways to fetch data from internet sources.",
-                    "link": ""
+                    "link": "https://nbviewer.jupyter.org/github/HiPyLiv/HiPyProject/blob/overhaul/resources/python/sourcing/%5BSOURCING%5D%20Importing%20Data%20From%20The%20Web/Importing%20Data%20From%20The%20Web.ipynb"
                 }
             }
         },
@@ -169,25 +163,25 @@ const resource_data = {
                     "id": 0,
                     "title": "Introduction to Statistical Analysis in Python",
                     "description": "A slow and steady introduction to doing statistics using Python, and the differences between statistics and machine learning.",
-                    "link": ""
+                    "link": "https://nbviewer.jupyter.org/github/HiPyLiv/HiPyProject/blob/overhaul/resources/python/statistics/%5BSTATISTICS%5D%20Introduction%20to%20Statistcal%20Analysis/Introduction%20to%20Statistical%20Analysis%20in%20Python.ipynb"
                 },
                 "Hypothesis Testing": {
                     "id": 1,
                     "title": "Hypothesis Testing in Python",
                     "description": "We look at how to perform perhaps the most common statistical query in Python - the hypothesis test, using scipy.stats.",
-                    "link": ""
+                    "link": "https://nbviewer.jupyter.org/github/HiPyLiv/HiPyProject/tree/overhaul/resources/python/statistics/%5BSTATISTICS%5D%20Hypothesis%20Testing%20In%20Python/"
                 },
                 "ANOVA": {
                     "id": 2,
                     "title": "ANOVA models in Python",
                     "description": "We look at more complex statistical models, developing our understanding of scipy.stats and introducing a new library: statsmodels",
-                    "link": ""
+                    "link": "https://nbviewer.jupyter.org/github/HiPyLiv/HiPyProject/blob/overhaul/resources/python/statistics/%5BSTATISTICS%5D%20ANOVA%20%28Analysis%20Of%20Variance%29/ANOVA%20%28Analysis%20Of%20Variance%29.ipynb"
                 },
                 "Regression": {
                     "id": 3,
                     "title": "Regression models in Python",
                     "description": "A very common statistical problem is finding a linear trend using known data. We learn how to do this using Python, scipy.stats and statsmodels.",
-                    "link": ""
+                    "link": "https://nbviewer.jupyter.org/github/HiPyLiv/HiPyProject/blob/overhaul/resources/python/statistics/%5BSTATISTICS%5D%20Statistical%20Regression/Statistical%20Regression.ipynb"
                 }
             }
         },
@@ -200,31 +194,31 @@ const resource_data = {
                     "id": 0,
                     "title": "Introduction to Machine Learning in Python",
                     "description": "We look at what we need to know to start our machine learning journey in Python, and how these methods differ from statistical ones.",
-                    "link": ""
+                    "link": "https://nbviewer.jupyter.org/github/HiPyLiv/HiPyProject/blob/overhaul/resources/python/machine%20learning/%5BML%5D%20Introduction%20to%20Machine%20Learning/Introduction%20to%20Machine%20Learning.ipynb"
                 },
                 "K Means Clustering": {
                     "id": 1,
                     "title": "K Means Clustering",
                     "description": "K Means Clustering is a very common clustering algorithm for grouping unlabelled data. Here we look at how it works and some applications",
-                    "link": ""
+                    "link": "https://nbviewer.jupyter.org/github/HiPyLiv/HiPyProject/blob/overhaul/resources/python/machine%20learning/%5BML%5D%20K%20Means%20Clustering/K%20Means%20Clustering.ipynb"
                 },
                 "K Nearest Neighbours": {
                     "id": 2,
                     "title": "K Nearest Neighbours",
                     "description": "The K Nearest Neighbours algorithm is a super simple algorithm for classification of data. We look at how it works, how to use it and how to choose a value of k.",
-                    "link": ""
+                    "link": "https://nbviewer.jupyter.org/github/HiPyLiv/HiPyProject/blob/overhaul/resources/python/machine%20learning/%5BML%5D%20K%20Nearest%20Neighbours/K%20Nearest%20Neighbours.ipynb"
                 },
                 "Decision Trees": {
                     "id": 3,
                     "title": "Decision Trees",
                     "description": "Decision trees are an intepretable model that has a unique classification footprint. Here we look at what they are and how to use them!",
-                    "link": ""
+                    "link": "https://nbviewer.jupyter.org/github/HiPyLiv/HiPyProject/blob/overhaul/resources/python/machine%20learning/%5BML%5D%20Decision%20Trees/Decision%20Trees.ipynb"
                 },
                 "SVM": {
                     "id": 4,
                     "title": "Support Vector Machines",
                     "description": "Support Vector Machines are classification models that split our feature space using a hyperplane. What does any of that mean, and how can we use it? We take a look!",
-                    "link": ""
+                    "link": "https://nbviewer.jupyter.org/github/HiPyLiv/HiPyProject/blob/overhaul/resources/python/machine%20learning/%5BML%5D%20Support%20Vector%20Machines/Support%20Vector%20Machines.ipynb"
                 }
             }
         }
